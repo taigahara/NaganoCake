e2517cd58447167f8f6781b2dfb7ae79a68459b1
taigahara <tigerhara@yahoo.ne.jp>
2020-11-04 12:59:37 +0000

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :end_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :public do
+    resource :end_users
+    root 'homes#top'
+    get 'homes/about'
+  end
 end
