977ecdef12edf8a16f39bb2f32e10595801f6555
原　大河 <tigerhara@yahoo.ne.jp>
2020-11-05 00:26:29 +0900

    とりあえず

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_homes_top_path
+    when EndUser
+      public_end_users_mypage_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :phone_number])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :end_users
+  root 'homes#top'
+  get 'homes/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :public do
     resource :end_users
-    root 'homes#top'
-    get 'homes/about'
+    get 'end_users/mypage' => 'end_users#show'
+  end
+
+  namespace :admin do
+    get 'homes/top'
   end
 end
