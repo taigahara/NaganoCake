825760428ced9a5c79d9e6b4e093987200763982
原　大河 <tigerhara@yahoo.ne.jp>
2020-11-20 11:38:21 +0900

    とりあえず

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -16,25 +16,14 @@ class Public::CartItemsController < ApplicationController
   end
 
   def add_item
-    binding.pry
-    if current_end_user.cart_items.find_by(product_id: params[:cart_item][:product_id])
-      cart_item = current_end_user.cart_items.find_by(product_id: params[:cart_item][:product_id])
-      cart_item.quantity = cart_item.quantity + params[:cart_item][:quantity].to_i
-    else
-      cart_item = CartItem.new(cart_add_params)
-      cart_item.end_user_id = current_end_user.id
-    end
-      cart_item.save
-      redirect_to cart_items_path
+    @cart_item = CartItem.new(cart_product_params)
+    @cart_item.save
+    redirect_to cart_items_path
   end
 
   private
-  def cart_add_params
-      params.require(:cart_item).permit(:product_id, :end_user_id, :quantity)
-  end
-
-  def cart_item_update
-      params.require(:cart_item).permit(:quantity)
+  def cart_product_params
+    params.require(:cart_item).permit(:quantity, :product_id, :end_user_id)
   end
 
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,6 +1,4 @@
 class CartItem < ApplicationRecord
   belongs_to :end_user
   belongs_to :product
-
-  attachment :image
 end
